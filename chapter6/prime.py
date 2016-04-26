4f484499acde9d7064110650aacb94e47863c0f8
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-04-26 13:02:56 -0400

    Chapter#6

--- chapter6/prime.py
+++ chapter6/prime.py
@@ -3,13 +3,11 @@ def main():
     print 'Number', number, 'is', is_prime(number), 'Number'
 
 def is_prime(n):
-        if n < 2:
-            return 'Not a Prime'
-        if n == 2:
-            return 'a Prime'
-        else:
-            for div in range(2, n):
-                if n % div == 0:
-                    return 'Not a Prime'
-                return 'a Prime'
+    if n >=2:
+        for i in range(2,n):
+            if not (n % i):
+                return 'Not a Prime'
+    else:
+        return 'Not a Prime'
+    return 'a Prime'
 main()
