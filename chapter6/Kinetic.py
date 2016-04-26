81559446cc34195d017c3cf1e2aeef48ca50bc30
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-04-25 22:07:35 -0400

    Chapter#6

--- chapter6/Kinetic.py
+++ chapter6/Kinetic.py
@@ -1,6 +1,8 @@
 def main():
     mass = float(raw_input("Enter the object's mass: "))
     velocity = float(raw_input("Enter the object's velocity: "))
-    kinetic_energy(mass, velocity)
+    print "The Kinetic Energy is", kinetic_energy(mass, velocity)
 def kinetic_energy(mass, velocity):
-    KE = 1.0/2
+    KE = 1.0/2 * mass * velocity**2
+    return KE
+main()
