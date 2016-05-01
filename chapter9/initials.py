37684f524fabf577300fe95525844360a654496a
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-04-30 21:03:00 -0400

    Chapter#9

---
+++ chapter9/initials.py
@@ -0,0 +1,6 @@
+def main():
+    first = raw_input("Enter the first name: ")
+    middle = raw_input("Enter the middle name: ")
+    last = raw_input("Enter the last name: ")
+    print first[0:1] + '.' + middle[0:1] + '.' + last[0:1]
+main()
