a9caba9ccaa9aec4e4a440b1f50bb74f13ad57b6
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-05-01 23:46:51 -0400

    Chapter#9

--- chapter9/initials.py
+++ chapter9/initials.py
@@ -2,5 +2,5 @@ def main():
     first = raw_input("Enter the first name: ")
     middle = raw_input("Enter the middle name: ")
     last = raw_input("Enter the last name: ")
-    print first[0:1] + '.' + middle[0:1] + '.' + last[0:1]
+    print first[0:2] + '.' + middle[0:2] + '.' + last[0:2]
 main()
