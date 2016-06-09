78e0b5ddf5f302f2507cf3092bdf0f3f0e4ae315
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-09 00:18:40 -0400

    Chapter#10

---
+++ chapter10/word_frequency.py
@@ -0,0 +1,11 @@
+#This program reads through a paragraph and count the word's frequencies and
+#returns it as a dictionary. words represents the key, and the number it appears
+#represents the value
+f = open('unique.txt','r').read().split()
+dictionary = {}
+for i in f:
+    if not i in dictionary:
+        dictionary[i] = 1
+    else:
+        dictionary[i] += 1
+print dictionary
