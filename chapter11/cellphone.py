d85a95945efd8cdff19fa92c7b9651b2fb56aeb7
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-13 22:21:21 -0400

    Chapter#11

---
+++ chapter11/cellphone.py
@@ -0,0 +1,24 @@
+class Cellphone:
+    def __init__(self, manufact, model, price):
+        self.__manufact = manufact
+        self.__model = model
+        self.__price = price
+
+    def set_manufact(self, manufact):
+        self.__manufact = manufact
+
+    def set_model(self, model):
+        self.__model = model
+
+    def set_retail_price(self, price):
+        self.__retail_price = price
+
+    def get_manufact(self):
+        return self.__manufact
+
+    def get_model(self):
+        return self.__model
+
+    def get_retail_price(self):
+        return self.__retail_price
+    
