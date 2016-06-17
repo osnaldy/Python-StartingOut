e160a0fa2a9fea8feb55ee781b30525b30d26106
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-17 14:08:14 -0400

    Chapter#11

---
+++ chapter11/car1.py
@@ -0,0 +1,14 @@
+class Car:
+
+    def __init__(self, year_model, make, speed):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+    def set_speed(self, speed):
+        self.__speed = speed + 5
+        return self.__speed
+
+    def set_break(self, speed):
+        self.__speed = speed - 5
+        return self.__speed
