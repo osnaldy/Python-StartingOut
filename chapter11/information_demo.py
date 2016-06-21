36c21a4eb348a34c970c7acd7b0d2d083ea7684e
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-20 22:31:19 -0400

    Chapter#11

--- chapter11/information_demo.py
+++ chapter11/information_demo.py
@@ -2,6 +2,16 @@ import information
 
 def main():
 
-    instance1 = information.Information('Tempo', '30 West Cleveland OHIO', '')
+    instance1 = information.Information('Tempo', '30 West Cleveland OHIO', '26', '212-212-2112')
+    print instance1
+    print
+
+    instance2 = information.Information('Ostempo', '361 West Cleveland OHIO', '30', '243-567-5437')
+    print instance2
+    print
+
+    instance3 = information.Information('Lantempo', '44 West Cleveland OHIO', '29', '212-212-8876')
+    print instance3
+    print
 
 main()
