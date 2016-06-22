c880393627a8b07ed5b2032c8975fe914ddab40c
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-21 21:39:37 -0400

    Chapter#12

--- chapter12/vehicles.py
+++ chapter12/vehicles.py
@@ -32,10 +32,15 @@ class Automobile:
     def get_price(self):
         return self.__price
 
-    
+class Car(Automobile):
 
+    def __init__(self, make, model, mileage, price, doors):
 
+        Automobile.__init__(self, make, model, mileage, price)
+        self.__doors = doors
 
+    def set_doors(self, doors):
+        self.__doors = doors
 
-
-
+    def get_doors(self):
+        return self.__doors
