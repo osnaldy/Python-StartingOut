e8dfed6ad2956922207a7e7d65c8c8be1fb3a1ad
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-23 14:31:56 -0400

    Chapter#13

--- chapter13/recursive_mult.py
+++ chapter13/recursive_mult.py
@@ -1,9 +1,12 @@
 def main():
 
-    recursive_multiplication(7, 4)
+    print recursive_multiplication(7, 4)
 
 def recursive_multiplication(x, y):
-    if x > 0:
-        recursive_multiplication(x -1, y)
-        print y
+
+   if x == 0:
+       return 0
+   elif x > 0:
+       return y + recursive_multiplication(x -1, y)
+
 main()
