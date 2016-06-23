54c5ba692bb2d0d701602316b362dcca91c77ad7
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-23 16:35:56 -0400

    Chapter#13

---
+++ chapter13/largest_list_item.py
@@ -0,0 +1,15 @@
+def main():
+
+    list = eval(raw_input("Enter a list of numbers: "))
+    print largest(list)
+    
+def largest(list):
+    if len(list) == 1:
+        return list[0]
+
+    large = largest(list[1:])
+
+    if large > list[0]:
+        return large
+    return list[0]
+main()
