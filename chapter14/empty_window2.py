e56e77b0b5e7dc82a38b5a3ec94800f845dc634e
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-24 19:41:23 -0400

    Chapter#14

--- chapter14/empty_window2.py
+++ chapter14/empty_window2.py
@@ -5,8 +5,10 @@ class GUI():
     def __init__(self):
         self.main_window = Tkinter.Tk()
 
-        self.label = Tkinter.Label(self.main_window, text='Hello People')
-        self.label.pack()
+        self.label1 = Tkinter.Label(self.main_window, text='Hello People')
+        self.label2 = Tkinter.Label(self.main_window, text= 'This is GUI')
+        self.label1.pack()
+        self.label2.pack()
         Tkinter.mainloop()
 
 my_Gui = GUI()
