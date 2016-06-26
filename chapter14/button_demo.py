b110da2b532528f4fd0cc7a41a7ff78e66a42516
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-26 10:30:11 -0400

    Chapter#14

--- chapter14/button_demo.py
+++ chapter14/button_demo.py
@@ -1,21 +1,20 @@
 import Tkinter
+
 import tkMessageBox
 
 class GUI():
 
     def __init__(self):
+
         self.main_window = Tkinter.Tk()
 
         self.my_button = Tkinter.Button(self.main_window,
-                                        text = 'Click Me!',
+                                        text = 'Click Me!!',
                                         command = self.do_something)
-
         self.my_button.pack()
 
         Tkinter.mainloop()
 
     def do_something(self):
-
-        tkMessageBox.showinfo('Response', 'Thanks for clicking')
-
+        tkMessageBox.showinfo('Response', 'FUCK YOU STONE')
 my_Gui = GUI()
