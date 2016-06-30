2c9fb645c3840449b830b49ac4fcdebf6ff99aed
Osnaldy Vasquez <osnaldy@hotmail.com>
2016-06-30 09:51:56 -0400

    Chapter#14

--- chapter14/employee.py
+++ chapter14/employee.py
@@ -13,25 +13,25 @@ class Employees():
         self.employee_label.grid(row = 0)
 
         self.employee_entry = Tkinter.Entry(self.main_window)
-        self.employee_entry.grid(row = 0, column = 1)
+        self.employee_entry.grid(row = 0, column = 3)
 
         self.id_number_label = Tkinter.Label(self.main_window, text = "Employee's ID_Number:")
         self.id_number_label.grid(row = 1)
 
         self.id_number_entry = Tkinter.Entry(self.main_window)
-        self.id_number_entry.grid(row =1 , column = 1)
+        self.id_number_entry.grid(row =1 , column = 3)
 
         self.department_label = Tkinter.Label(self.main_window, text = "Employee's Department:")
         self.department_label.grid(row =2)
 
         self.department_entry = Tkinter.Entry(self.main_window)
-        self.department_entry.grid(row =2 , column = 1)
+        self.department_entry.grid(row =2 , column = 3)
 
         self.job_title_label = Tkinter.Label(self.main_window, text = "Employee's Job Title:")
         self.job_title_label.grid(row =3)
 
         self.job_title_entry = Tkinter.Entry(self.main_window)
-        self.job_title_entry.grid(row =3 , column = 1)
+        self.job_title_entry.grid(row =3 , column = 3)
 
         self.enter_button = Tkinter.Button(self.main_window, text = 'Enter Information',
                                            command = self.add_employee)
@@ -39,7 +39,11 @@ class Employees():
 
         self.exit_button = Tkinter.Button(self.main_window, text = 'Exit Program',
                                           command = self.main_window.destroy)
-        self.exit_button.grid(row = 5, column = 1)
+        self.exit_button.grid(row = 5, column = 3)
+
+        self.look_button = Tkinter.Button(self.main_window, text = "Look Up Employee",
+                                          command = self.look_up)
+        self.look_button.grid(row = 7, column = 2)
 
         self.employee = {}
 
@@ -59,5 +63,13 @@ class Employees():
         self.department_entry.delete(0, "end")
         self.job_title_entry.delete(0, "end")
 
+    def look_up(self):
+
+        self.new_window = Tkinter.Tk()
+
+        self.label_1 = Tkinter.Button(self.new_window, text = 'Employee Name')
+        self.label_1.grid(row = 0)
+
+        Tkinter.mainloop()
 
 gui = Employees()
